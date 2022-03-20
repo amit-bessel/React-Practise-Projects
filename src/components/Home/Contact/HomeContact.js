61465a26d27fe623c1db77f32b829e90f787a2c4
Amit <amit.unified@gmail.com>
2022-03-20 23:04:02 +0530

    Change in Contact form

--- src/components/Home/Contact/HomeContact.js
+++ src/components/Home/Contact/HomeContact.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 
 function HomeContact() {
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors, isValid, isSubmitted, isSubmitSuccessful }, handleSubmit } = useForm();
     const onSubmit = (data) => {
+       
         alert(JSON.stringify(data));
     };
 
@@ -20,37 +21,45 @@ function HomeContact() {
             </div>
             <div className="divider-custom-line"></div>
         </div>
+        {isValid && isSubmitSuccessful && (<div id="submitSuccessMessage">
+            <div className="text-center mb-3">
+                <div className="fw-bolder"> Form submission successful! </div>
+            </div>
+        </div>)}
 
+        {!isValid && isSubmitSuccessful && (<div id="submitErrorMessage">
+            <div className="text-center text-danger mb-3">
+            Error sending message!
+            </div>
+        </div>)}
+        
+        
         <div className="row justify-content-center">
             <div className="col-lg-8 col-xl-7">
                 <form id="contactForm" autoComplete='Off' onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-floating mb-3">
-                    <input className="form-control" name="name" type="text" placeholder="Enter your name..." {...register("name")} />
+                    <input className="form-control" name="name" type="text" placeholder="Enter your name..." {...register("name", { required: true })} />
                     <label htmlFor="name">Full name</label>
-                    {/* <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div> */}
+                    { errors?.name && (<div className="invalid-feedback d-block">A name is required.</div>)}
                 </div>
                 <div className="form-floating mb-3">
-                    <input className="form-control" name="email" type="email" placeholder="name@example.com" {...register("email")}/>
+                    <input className="form-control" name="email" type="email" placeholder="name@example.com" {...register("email", { required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  })}/>
                     <label htmlFor="email">Email address</label>
+                    {errors?.email?.type === "required" && (<div className="invalid-feedback d-block">Email is required.</div>)}
+                    {errors?.email?.type === "pattern" && (<div className="invalid-feedback d-block">Email pattern is wrong.</div>)}
                 </div>
                 <div className="form-floating mb-3">
-                    <input className="form-control" name="phone" type="tel" placeholder="(123) 456-7890" {...register("phone")} />
+                    <input className="form-control" name="phone" type="tel" placeholder="(123) 456-7890" {...register("phone", { required: true, pattern: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/i})} />
                     <label htmlFor="phone">Phone number</label>
+                    {errors?.phone?.type === "required" && (<div className="invalid-feedback d-block">Phone is required.</div>)}
+                    {errors?.phone?.type === "pattern" && (<div className="invalid-feedback d-block">Phone pattern is wrong.</div>)}
                 </div>
                 <div className="form-floating mb-3">
-                    { <textarea className="form-control" id="message" type="text" {...register("message")}></textarea>}
+                    { <textarea className="form-control" id="message" type="text" {...register("message", { required: true })}></textarea>}
                     <label htmlFor="message">Message</label>
+                    { errors?.message && (<div className="invalid-feedback d-block">Message is required.</div>)}
                 </div>
-                <div className="" id="submitSuccessMessage">
-                    <div className="text-center mb-3">
-                        <div className="fw-bolder"> Form submission successful! </div>
-                    </div>
-                </div>
-                <div className="d-none" id="submitErrorMessage">
-                    <div className="text-center text-danger mb-3">
-                    Error sending message!
-                    </div>
-                </div>
+                
                 <button className="btn btn-primary btn-xl" id="submitButton" type="submit">Send</button>
                 </form>
             </div>
